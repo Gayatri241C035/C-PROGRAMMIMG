575b2c000a4f4ddc82bccd5d7d51f5b89f25509e
Gayatri241C035 <gayatri@eng.rizvi.edu.in>
2024-11-24 18:47:50 +0530

    Create EXP1.1.C

---
+++ EXP1.1.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    printf("Full Name\t: GAYATRI GODAMBE\n");
+    printf("Age\t\t: 18\n");
+    printf("Class\t\t: FE\n");
+    printf("Division\t: B\n");
+    printf("UIN\t\t: 241C035\n");
+
+    return 0;
+}
