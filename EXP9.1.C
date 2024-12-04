925f859a2a4dad998304b3f44735aa6155552367
Gayatri241C035 <gayatri@eng.rizvi.edu.in>
2024-12-04 22:26:59 +0530

    Create EXP9.1.C

---
+++ EXP9.1.C
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+
+// Defining the structure to hold student data
+struct student_record {
+    char name[50];
+    int roll_number;
+    float total_marks;
+};
+
+// Function to read student data
+void input_student_data(struct student_record students[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("Enter details for student %d:\n", i + 1);
+        
+        printf("Name: ");
+        getchar(); // To consume the newline left by the previous input
+        gets(students[i].name);
+        
+        printf("Roll Number: ");
+        scanf("%d", &students[i].roll_number);
+
+        printf("Total Marks: ");
+        scanf("%f", &students[i].total_marks);
+   }
+}
+
+// Function to find and return the index of the student with the highest marks
+int find_topper(struct student_record students[], int n) {
+    int topper_index = 0;
+    for (int i = 1; i < n; i++) {
+        if (students[i].total_marks > students[topper_index].total_marks) {
+            topper_index = i;
+        }
+    }
+    return topper_index;
+}
+
